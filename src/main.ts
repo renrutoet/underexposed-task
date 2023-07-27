cd41b482b33f6ab2f2de571e11493cd259b80661
Teo Turner <teoturner@gmail.com>
2023-07-27 20:23:08 +0100

    chore: removed unused data

--- src/main.ts
+++ src/main.ts
@@ -5,44 +5,5 @@ import "./styles/animations.css";
 import "@fontsource/goldman";
 import "@fontsource-variable/plus-jakarta-sans";
 import { renderApp } from "./renderUtils/rendering";
-import { F1TemplateData } from "./renderUtils/templates";
-
-export const testData = [
-    {
-        round: "Round 1",
-        dates: "25-27 MAR",
-        flag: "FLAG",
-        country: "Saudi Arabia",
-        series: "STC SAUDI ARABIAN GRAND PRIX 2022",
-    },
-    {
-        round: "Round 2",
-        dates: "08-10 APR",
-        flag: "FLAG",
-        country: "Australia",
-        series: "FORMULA 1 HEINEKEN AUSTRALIAN GRAND PRIX 2022 FORMULA 1 HEINEKEN AUSTRALIAN GRAND PRIX 2022FORMULA 1 HEINEKEN AUSTRALIAN GRAND PRIX 2022FORMULA 1 HEINEKEN AUSTRALIAN GRAND PRIX 2022",
-    },
-    {
-        round: "Round 2",
-        dates: "08-10 APR",
-        flag: "FLAG",
-        country: "Australia",
-        series: "FORMULA 1 HEINEKEN AUSTRALIAN GRAND PRIX 2022",
-    },
-    {
-        round: "Round 2",
-        dates: "08-10 APR",
-        flag: "FLAG",
-        country: "Australia",
-        series: "FORMULA 1 HEINEKEN AUSTRALIAN GRAND PRIX 2022",
-    },
-    {
-        round: "Round 2",
-        dates: "08-10 APR",
-        flag: "FLAG",
-        country: "Australia",
-        series: "FORMULA 1 HEINEKEN AUSTRALIAN GRAND PRIX 2022",
-    },
-] as F1TemplateData[];
 
 renderApp();
