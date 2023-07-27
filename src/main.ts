b8b6810937a8781dc547f66f288f43026252da9a
Teo Turner <teoturner@gmail.com>
2023-07-27 20:21:35 +0100

    feat: added simple animations

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import "./style.css";
 import "./styles/card.css";
 import "./styles/viewPage.css";
+import "./styles/animations.css";
 import "@fontsource/goldman";
 import "@fontsource-variable/plus-jakarta-sans";
 import { renderApp } from "./renderUtils/rendering";
